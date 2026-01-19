303a874553164c065b4425d3a23aa24c3b7f7726
ClaireLC <claire@atomos.systems>
2026-01-19 13:30:28 -0700

    Robot state subscriber

--- xarm_moveit_servo/src/xarm_track_pose.cpp
+++ xarm_moveit_servo/src/xarm_track_pose.cpp
@@ -6,7 +6,6 @@
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <moveit_msgs/srv/servo_command_type.hpp>
 
-
 #include <xarm_msgs/msg/robot_msg.hpp>
 
 /**
@@ -76,72 +75,101 @@ geometry_msgs::msg::PoseStamped getPose(const Eigen::Vector3d& position, const E
   return target_pose;
 }
 
-void robotStateCallback(const xarm_msgs::msg::RobotMsg::SharedPtr msg)
-{
-  RCLCPP_INFO_STREAM(rclcpp::get_logger("robot_state"), "Robot state");
-}
+//void robotStateCallback(const xarm_msgs::msg::RobotMsg::SharedPtr msg)
+//{
+//  RCLCPP_INFO(rclcpp::get_logger("robot_state"), "Received robot state");
+//}
 
-int main(int argc, char* argv[])
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<xarm_msgs::msg::RobotMsg>(
+      "/ufactory/robot_states", rclcpp::SystemDefaultsQoS(), std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const xarm_msgs::msg::RobotMsg::SharedPtr msg) const
+    // Print 6 float values in msg->pose
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard robot state: %f, %f, %f, %f, %f, %f", msg->pose[0], msg->pose[1], msg->pose[2], msg->pose[3], msg->pose[4], msg->pose[5]);
+    }
+    rclcpp::Subscription<xarm_msgs::msg::RobotMsg>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("servo_tutorial");
-
-  // Publishers
-  auto marker_publisher =
-      node->create_publisher<visualization_msgs::msg::MarkerArray>("/visualization_marker_array", rclcpp::SystemDefaultsQoS());
-  auto pose_publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("/servo_server/pose_cmds",
-                                                                                rclcpp::SystemDefaultsQoS());
-  // Subscribers
-  auto robot_state_subscriber = node->create_subscription<xarm_msgs::msg::RobotMsg>("/ufactory/robot_states", rclcpp::SystemDefaultsQoS(), std::bind(&robotStateCallback, std::placeholders::_1));
-
-  // Service clients
-  auto switch_input_client = node->create_client<moveit_msgs::srv::ServoCommandType>("/servo_server/switch_command_type");
-
-  auto executor = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-  executor->add_node(node);
-
-  // Spin the node.
-  std::thread executor_thread([&executor]() { executor->spin(); });
-
-  // Generate path
-  std::vector<Eigen::Vector3d> path = getPath();
-
-  // Switch to POSE input type
-  switch_input_client = node->create_client<moveit_msgs::srv::ServoCommandType>("/servo_server/switch_command_type");
-  auto request = std::make_shared<moveit_msgs::srv::ServoCommandType::Request>();
-  request->command_type = moveit_msgs::srv::ServoCommandType::Request::POSE;
-  if (switch_input_client->wait_for_service(std::chrono::seconds(5)))
-  {
-    auto result = switch_input_client->async_send_request(request);
-    if (result.get()->success)
-    {
-      RCLCPP_INFO_STREAM(node->get_logger(), "Switched to input type: POSE");
-    }
-    else
-    {
-      RCLCPP_WARN_STREAM(node->get_logger(), "Could not switch input to: POSE");
-    }
-  }
-  else
-  {
-    RCLCPP_ERROR_STREAM(node->get_logger(), "Failed to call service /servo_server/switch_command_type");
-  }
-
-  //// Follow the trajectory
-  //const double publish_period = 0.15;
-  //rclcpp::WallRate rate(1.0 / publish_period);
-  //for (auto& waypoint : path)
-  //{
-  //  auto target_pose = getPose(waypoint, Eigen::Quaterniond(ee_pose.rotation()));
-  //  target_pose.header.stamp = node->now();
-  //  pose_publisher->publish(target_pose);
-  //  rate.sleep();
-  //}
-
-  //executor->cancel();
-  //if (executor_thread.joinable())
-  //{
-  //  executor_thread.join();
-  //}
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
   rclcpp::shutdown();
+  return 0;
 }
+
+//int main(int argc, char* argv[])
+//{
+//  rclcpp::init(argc, argv);
+//  rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("servo_tutorial");
+//
+//  // Publishers
+//  auto marker_publisher =
+//      node->create_publisher<visualization_msgs::msg::MarkerArray>("/visualization_marker_array", rclcpp::SystemDefaultsQoS());
+//  auto pose_publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("/servo_server/pose_cmds",
+//                                                                                rclcpp::SystemDefaultsQoS());
+//  // Subscribers
+//  auto robot_state_subscriber = node->create_subscription<xarm_msgs::msg::RobotMsg>("/ufactory/robot_states", rclcpp::SystemDefaultsQoS(), std::bind(&robotStateCallback, std::placeholders::_1));
+//
+//  // Service clients
+//  auto switch_input_client = node->create_client<moveit_msgs::srv::ServoCommandType>("/servo_server/switch_command_type");
+//
+//  auto executor = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+//  executor->add_node(node);
+//
+//  // Spin the node.
+//  std::thread executor_thread([&executor]() { executor->spin(); });
+//
+//  // Generate path
+//  std::vector<Eigen::Vector3d> path = getPath();
+//
+//  // Switch to POSE input type
+//  switch_input_client = node->create_client<moveit_msgs::srv::ServoCommandType>("/servo_server/switch_command_type");
+//  auto request = std::make_shared<moveit_msgs::srv::ServoCommandType::Request>();
+//  request->command_type = moveit_msgs::srv::ServoCommandType::Request::POSE;
+//  if (switch_input_client->wait_for_service(std::chrono::seconds(5)))
+//  {
+//    auto result = switch_input_client->async_send_request(request);
+//    if (result.get()->success)
+//    {
+//      RCLCPP_INFO_STREAM(node->get_logger(), "Switched to input type: POSE");
+//    }
+//    else
+//    {
+//      RCLCPP_WARN_STREAM(node->get_logger(), "Could not switch input to: POSE");
+//    }
+//  }
+//  else
+//  {
+//    RCLCPP_ERROR_STREAM(node->get_logger(), "Failed to call service /servo_server/switch_command_type");
+//  }
+//
+//  //// Follow the trajectory
+//  //const double publish_period = 0.15;
+//  //rclcpp::WallRate rate(1.0 / publish_period);
+//  //for (auto& waypoint : path)
+//  //{
+//  //  auto target_pose = getPose(waypoint, Eigen::Quaterniond(ee_pose.rotation()));
+//  //  target_pose.header.stamp = node->now();
+//  //  pose_publisher->publish(target_pose);
+//  //  rate.sleep();
+//  //}
+//
+//  executor->cancel();
+//  if (executor_thread.joinable())
+//  {
+//    executor_thread.join();
+//  }
+//  rclcpp::shutdown();
+//}
